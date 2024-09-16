c2c73c50b8aa74c4e374208c4fd15e1657ffa3a2
SuperWD40 <matteo.bernard@outlook.fr>
2024-09-16 08:51:59 +0200

    2

--- index_toolkit.py
+++ index_toolkit.py
@@ -6,127 +6,99 @@ import ipywidgets as widgets
 from IPython.display import display
 import matplotlib.pyplot as plt
 import matplotlib.ticker
-
 import warnings
 warnings.filterwarnings("ignore")
 
 class BaseClass:
-    _date = None
-    _range_dict = None
-    _freq_dict = None
-    _range_keys = None
-    _freq_keys = None
-    
-    def __init__(self, date):
-        self.date = pd.to_datetime(date, dayfirst=True).tz_localize(None)
-        if BaseClass._date is None:
-            BaseClass._initialize_shared_data(self.date)
+    def __init__(self, input_date): 
+        self.date = pd.to_datetime(input_date, dayfirst=True).tz_localize(None)
 
-    @classmethod
-    def _initialize_shared_data(cls, date):
-        cls._range_dict = {
-            '1W'    : date - pd.Timedelta(days=7),
-            '1M'    : date - pd.Timedelta(days=30),
-            'YTD'   : pd.Timestamp(year=date.year, month=1, day=1),
-            '1Y'    : date - pd.Timedelta(days=365),
-            '5Y'    : date - pd.Timedelta(days=365*5),
-            '10Y'   : date - pd.Timedelta(days=365*10),
+    def get_range_dict(self):  
+        date = self.date 
+        return {
+            '1W': date - pd.Timedelta(days=7),
+            '1M': date - pd.Timedelta(days=30),
+            'YTD': pd.Timestamp(year=date.year, month=1, day=1),
+            '1Y': date - pd.Timedelta(days=365),
+            '5Y': date - pd.Timedelta(days=365 * 5),
+            '10Y': date - pd.Timedelta(days=365 * 10),
         }
-        cls._freq_dict = {
-            'D' : 365,
-            'B' : 255,
-            'W' : 52,
-            'M' : 12
+
+    def get_freq_dict(self):
+        return {
+            'D': 365,
+            'B': 255,
+            'W': 52,
+            'M': 12
         }
-        cls._range_keys = list(cls._range_dict.keys())
-        cls._freq_keys = list(cls._freq_dict.keys())
-        cls._date = date
-    
-    @classmethod
-    def get_range_dict(cls):
-        return cls._range_dict
-    
-    @classmethod
-    def get_freq_dict(cls):
-        return cls._freq_dict
-    
-    @classmethod
-    def get_range_keys(cls):
-        return cls._range_keys
-    
-    @classmethod
-    def get_freq_keys(cls):
-        return cls._freq_keys
-    
-    @classmethod
-    def get_date(cls):
-        return cls._date
 
-# Calculating asset valuations and allocation
-class index_table(BaseClass):
-    def __init__(self, history, allocation, date):
-        super().__init__(date)
-        # Assigning input parameters to instance variables
-        self.history = history
-        self.date = self.get_date()
-        self.allocation = allocation
+    def get_date(self): 
+        return self.date
 
-    def compute(self):
+    def get_history(self, input_index, input_history, freq='B', range='1Y', by='All'):
+        #Index
         index = pd.DataFrame()
-        index.index = self.allocation.index
-        index['Price'] = self.history.loc[self.date]
-        index['Share'] = self.allocation['Share']
-        index['Valorisation'] = self.allocation['Share'] * index['Price']
+        index.index = input_index.index
+        index['Price'] = input_history.loc[self.date]
+        index['Share'] = input_index['Share']
+        index['Valorisation'] = input_index['Share'] * index['Price']
         index['Allocation'] = index['Valorisation'] / index['Valorisation'].sum()
-        index['Date'] = self.date
-        index['Type'] = self.allocation['Type']
-        index['Account'] = self.allocation['Account']
-        return index
-    
-    def historization(self):
-        
-    
-    def show(self):
-        display = self.compute()
-        display = display.drop(columns=['Date'])
-        display['Allocation'] = display['Allocation'].apply(lambda x: f"{x * 100:.2f}%")
+        if by not in ['All', 'Overall']:
+            index[by] = input_index[by]
+            by_index = index.groupby(by)['Allocation'].transform('sum')
+            index['Allocation'] = index['Allocation'] / by_index
 
-        with pd.option_context(
-            'display.max_rows', None,
-            'display.max_columns', None,
-            'display.float_format', '{:,.1f}'.format,
-            'display.max_colwidth', None,
-            'display.colheader_justify', 'center'
-        ):
-            print(display)
+        #History
+        input_history = input_history.loc[range:]
+        input_history = input_history.resample(freq).ffill()
+        if by == 'All':
+            history = input_history
+        elif by == 'Overall':
+            history = (index['Allocation'] * input_history.pct_change()).T.sum()
+            history = 100 * (history + 1).cumprod()
+            history.name = 'Overall'
+            history = pd.DataFrame(history)
+        else:
+            history = pd.DataFrame()
+            for n in index[by].unique():
+                allocation = index[index[by] == n]['Allocation']
+                by_history = input_history[index[index[by] == n].index]
+                by_history = (allocation * input_history.pct_change()).T.sum()
+                by_history = 100 * (by_history + 1).cumprod()
+                by_history.name = n
+                history = pd.concat([history, by_history], axis=1)
+                    
+        return history
 
 # Class Composition
 class stats_table(BaseClass):
-    def __init__(self, history, market, riskfree, allocation, date):
-        super().__init__(date)
-        # Assigning input parameters to instance variables
-        self.history = history
+    def __init__(self, history, index, market, riskfree, date):
+        super().__init__(date) 
         self.date = self.get_date()
         self.range_dict = self.get_range_dict()
-        self.freq_dict = self.get_freq_dict()
-        self.range_keys = self.get_range_keys()
-        self.freq_keys = self.get_freq_keys()
-        self.market = market
-        self.riskfree = riskfree
-        self.allocation = allocation
-    
-    def stats(self, freq, range): 
+        self.freq_dict = self.get_freq_dict() 
+
+        self.input_index = index
+        self.input_history = history
+        self.input_market = market
+        self.input_riskfree = riskfree
+
+    def stats(self, freq, range, by): 
         """Calculating statistics based on frequency and time range"""
         # Fetching historical data and resampling based on frequency and time range
-        df_data = self.history
-        df_data = df_data.loc[self.range_dict[range]:]
-        df_data = df_data.resample(freq).ffill()
+        df_data = self.get_history(
+            input_index=self.input_index, 
+            input_history=self.input_history, 
+            freq=freq,
+            range=self.range_dict[range],
+            by=by
+        )
 
         # Fetching market data and risk-free rate for the specified time range and frequency
-        market = self.market
+        market = self.input_market
         market = market.loc[self.range_dict[range]:]
         market = market.resample(freq).ffill()
-        riskfree = self.riskfree
+        riskfree = self.input_riskfree
 
         # Get the correct freq for a given freq
         freq = self.freq_dict[freq]
@@ -155,10 +127,13 @@ class stats_table(BaseClass):
                 df_stats.at['Skewness', ticker]         = "{:.1f}".format(stats.kurtosis(history))
                 df_stats.at['Kurtosis', ticker]         = "{:.1f}".format(stats.skew(history))
                 #df_stats.at['3% VAR', ticker]           = "{:.1f}%".format(finance.var(history, feq='B', conf_level=0.03) * 100)
-                df_stats.at['Allocation', ticker]       = "{:.1f}%".format(self.allocation[ticker] * 100)
+                #df_stats.at['Allocation', ticker]       = "{:.1f}%".format(self.allocation[ticker] * 100)
 
         # Displaying the statistics table
-        with pd.option_context('expand_frame_repr', False):
+        with pd.option_context(
+            'expand_frame_repr', None,
+            'display.colheader_justify', 'center'
+        ):
             print(df_stats)
         
     def show(self):
@@ -166,8 +141,9 @@ class stats_table(BaseClass):
         # Creating interactive controls for selecting frequency and time range
         controls = widgets.interactive(
             self.stats,
-            freq=widgets.Select(options=self.freq_keys, value=self.freq_keys[0]),
-            range=widgets.Select(options=self.range_keys, value=self.range_keys[0])
+            freq=widgets.Select(options=list(self.freq_dict.keys()), value='B'),
+            range=widgets.Select(options=list(self.range_dict.keys()), value='1M'),
+            by=widgets.Select(options=['All', 'Overall', 'Type', 'Account'], value='All')
         )
 
         # Displaying the interactive controls
